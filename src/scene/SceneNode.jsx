71080f0f1f60cb6b82ba5db755c3597ee1a31fe9
Scrabx3 <kris.vlntn@gmail.com>
2023-05-07 00:54:04 +0200

    center stage name, port design

--- src/scene/SceneNode.jsx
+++ src/scene/SceneNode.jsx
@@ -7,6 +7,11 @@ import './SceneNode.css'
 
 const NODE_HEIGHT = 100;
 const NODE_WIDTH = 180;
+const START_COLOR = 'rgb(0, 255, 0, 0.8)';
+const PORT_DEFAULTS = {
+  fill: 'rgb(240, 248, 255, 0.3)',
+  stroke: 'black',
+}
 
 function makeMenuItem(label, key, disabled, danger) {
   return { key, label, disabled, danger };
@@ -19,13 +24,14 @@ function makeColor(r, g, b, a = 1) {
 function FixedLength(props) {
   const fixedLen_svg = () => (
     <svg viewBox="112 176 800 672" width="1em" height="1em" fill="currentColor">
-      <path d="M 180 176 h -60 c -4.4 0 -8 3.6 -8 8 v 656 c 0 4.4 3.6 8 8 8 h 60 c 4.4 0 8 -3.6 8 -8 V 184 c 0 -4.4 -3.6 -8 -8 -8 z m 724 0 h -60 c -4.4 0 -8 3.6 -8 8 v 656 c 0 4.4 3.6 8 8 8 h 60 c 4.4 0 8 -3.6 8 -8 V 184 c 0 -4.4 -3.6 -8 -8 -8 z M 785.3 504.3 L 657.7 403.6 a 7.23 7.23 0 0 0 -11.7 5.7 V 476 H 238 V 548 h 407.3 v 62.8 c 0 6 7 9.4 11.7 5.7 l 127.5 -100.8 c 3.8 -2.9 3.8 -8.5 0.2 -11.4 z"/>
+      <path d="M 180 176 h -60 c -4.4 0 -8 3.6 -8 8 v 656 c 0 4.4 3.6 8 8 8 h 60 c 4.4 0 8 -3.6 8 -8 V 184 c 0 -4.4 -3.6 -8 -8 -8 z m 724 0 h -60 c -4.4 0 -8 3.6 -8 8 v 656 c 0 4.4 3.6 8 8 8 h 60 c 4.4 0 8 -3.6 8 -8 V 184 c 0 -4.4 -3.6 -8 -8 -8 z M 785.3 504.3 L 657.7 403.6 a 7.23 7.23 0 0 0 -11.7 5.7 V 476 H 238 V 548 h 407.3 v 62.8 c 0 6 7 9.4 11.7 5.7 l 127.5 -100.8 c 3.8 -2.9 3.8 -8.5 0.2 -11.4 z" />
     </svg>
   );
   return (
     <Icon component={fixedLen_svg} {...props} />
   )
 }
+
 
 function StageNode({ node, graph }) {
   const [hovered, setHover] = useState(false);
@@ -42,11 +48,14 @@ function StageNode({ node, graph }) {
       fixedLen ? makeColor(175, 235, 255, 0.9) :
         undefined;
 
+  node.prop('ports/groups/default/attrs/path/stroke', start ? START_COLOR : PORT_DEFAULTS.stroke);
+  node.prop('ports/groups/default/attrs/path/fill', color ? color : PORT_DEFAULTS.fill);
+
   const contextItems = [
     makeMenuItem('Edit', 'edit'),
     makeMenuItem('Clone', 'clone'),
     { type: "divider" },
-    makeMenuItem('Mark as root', 'makeroot', ),
+    makeMenuItem('Mark as root', 'makeroot',),
     makeMenuItem('Remove connections', 'removeconnections'),
     { type: "divider" },
     makeMenuItem('Delete', 'remove', false, true),
@@ -92,9 +101,9 @@ function StageNode({ node, graph }) {
         className="stage-content"
         style={{
           backgroundColor: color,
-          borderColor: start ? makeColor(0, 255, 0, 0.8) : undefined,
+          borderColor: start ? START_COLOR : undefined,
         }}>
-        <Row>
+        <Row className='node-header'>
           <Space className='node-attribute-icons' size={10} wrap={false}>
             {!navText ?
               <Tooltip title={'Missing navigation text'}>
@@ -114,25 +123,27 @@ function StageNode({ node, graph }) {
               </Tooltip> : <></>}
           </Space>
           <div style={hovered ? {} : { display: 'none' }}>
-            <Space.Compact className="node-controll-button-holder">
+            <Space.Compact className="node-controll-button-holder" size='small'>
               <Tooltip title={'Edit'} mouseEnterDelay={0.5}>
-                <Button size='small' type='text' onClick={() => { editStage() }} icon={<EditOutlined />} />
+                <Button type='text' onClick={() => { editStage() }} icon={<EditOutlined />} />
               </Tooltip>
               <Tooltip title={'Clone'} mouseEnterDelay={0.5}>
-                <Button size='small' type='text' onClick={() => { cloneStage() }} icon={<CopyOutlined />} />
+                <Button type='text' onClick={() => { cloneStage() }} icon={<CopyOutlined />} />
               </Tooltip>
               <Tooltip title={'Delete'} mouseEnterDelay={0.5}>
-                <Button size='small' type='text' onClick={() => { node.remove() }} icon={<CloseOutlined style={{ color: 'red' }} />} />
+                <Button type='text' onClick={() => { node.remove() }} icon={<CloseOutlined style={{ color: 'red' }} />} />
               </Tooltip>
             </Space.Compact>
           </div>
         </Row>
-        <Row>
-          <h3>{label ?
-            label
+        <div className='stage-name'>
+          <h4>{label ?
+            <Tooltip title={label}>
+              {label}
+            </Tooltip>
             : 'Untitled'}
-          </h3>
-        </Row>
+          </h4>
+        </div>
       </div>
     </Dropdown>
   );
@@ -147,32 +158,29 @@ register({
       default: {
         markup: [
           {
-            tagName: 'rect',
-            selector: 'portBodySide'
+            tagName: 'path',
+            selector: 'path',
           },
         ],
         attrs: {
-          portBodySide: {
-            width: 10,
-            height: NODE_HEIGHT - 50,
-            strokeWidth: 2,
-            stroke: '#222431',
-            fill: '#EFF4FF',
+          path: {
+            d: 'M 0 -40 L 10 0 L 0 40 z',
             magnet: true,
+            stroke: PORT_DEFAULTS.stroke,
+            strokeWidth: 1,
+            fill: PORT_DEFAULTS.fill,
           },
         },
-        position: {
-          name: 'right',
-          args: {
-            dy: - NODE_HEIGHT / 2 + 30,
-            dx: - 5
-          }
-        }
+        position: 'absolute'
       },
     },
     items: [
       {
         group: 'default',
+        args: {
+          x: (NODE_WIDTH - 1.0),
+          y: '50%'
+        },
       },
     ]
   },
