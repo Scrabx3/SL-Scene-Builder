ce3eeb94efb3b7f864fc1764eab0ee0f16fd96d0
Scrabx3 <kris.vlntn@gmail.com>
2023-07-07 21:42:26 +0200

    undo float multiplier magnitude change

--- src-tauri/src/define/serialize.rs
+++ src-tauri/src/define/serialize.rs
@@ -21,13 +21,13 @@ impl EncodeBinary for Offset {
     }
 
     fn write_byte(&self, buf: &mut Vec<u8>) -> () {
-        let x_ = (self.x * 100000.0).round() as i32;
+        let x_ = (self.x * 1000.0).round() as i32;
         buf.extend_from_slice(&x_.to_be_bytes());
-        let y_ = (self.y * 100000.0).round() as i32;
+        let y_ = (self.y * 1000.0).round() as i32;
         buf.extend_from_slice(&y_.to_be_bytes());
-        let z_ = (self.z * 100000.0).round() as i32;
+        let z_ = (self.z * 1000.0).round() as i32;
         buf.extend_from_slice(&z_.to_be_bytes());
-        let r_ = (self.r * 100000.0).round() as i32;
+        let r_ = (self.r * 1000.0).round() as i32;
         buf.extend_from_slice(&r_.to_be_bytes());
     }
 }
